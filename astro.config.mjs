371bca6a3fa64d9477accc2dc778a11a4262da72
david-rosval <20b3010135@untels.edu.pe>
2024-09-05 17:41:06 -0500

    desplegando con netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,12 @@ import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
 
-import vercel from '@astrojs/vercel/serverless';
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
   integrations: [mdx(), sitemap()],
   output: 'server',
-  adapter: vercel(),
+  adapter: netlify(),
 });
